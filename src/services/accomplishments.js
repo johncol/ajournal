00b5028ae98cf1b97223834035d297b13ceb512e
John <john.19col@gmail.com>
2020-04-19 13:42:23 -0500

    Small styling update

--- src/services/accomplishments.js
+++ src/services/accomplishments.js
@@ -3,7 +3,7 @@ export const getAccomplishmentsPerDay = (accomplishments) => {
 
   accomplishments.forEach((accomplishment) => {
     const date = new Date(accomplishment.timestamp);
-    const mapKey = date.toISOString().substring(0, 10);
+    const mapKey = date.toDateString().substring(0, 10);
 
     const day = accomplishmentsPerDay[mapKey] ?? {
       date: date.toDateString(),
@@ -14,7 +14,7 @@ export const getAccomplishmentsPerDay = (accomplishments) => {
     accomplishmentsPerDay[mapKey] = day;
   });
 
-  return Object.keys(accomplishmentsPerDay)
-    .sort((date1, date2) => (date1 < date2 ? 1 : -1))
-    .map((date) => accomplishmentsPerDay[date]);
+  return Object.keys(accomplishmentsPerDay).map(
+    (date) => accomplishmentsPerDay[date]
+  );
 };
