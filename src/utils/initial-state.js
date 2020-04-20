bdd320f8d774b0fc6c17bafc3e7c415ff1e671a2
John <john.19col@gmail.com>
2020-04-19 19:17:20 -0500

    Add entry to initial data file

--- src/utils/initial-state.js
+++ src/utils/initial-state.js
@@ -3,4 +3,8 @@ export const startingAccomplishments = [
     timestamp: 1587271862678,
     description: 'Completed the first version of AJournal',
   },
+  {
+    timestamp: 1587341781631,
+    description: 'Deploy AJournal SPA in AWS (AWS Amplify)',
+  },
 ];
